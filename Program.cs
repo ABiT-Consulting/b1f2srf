24a470fc19a502da502282998620bff975782e18
Muhammad Imran <i_muhmmad@hotmail.com>
2022-06-11 14:33:47 +0500

    initial version

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+var xml = System.IO.File.ReadAllText("b1iF.xml");
+var docBIF = XDocument.Parse(xml);
+//Console.WriteLine(docBIF.ToString());
+
+var appformnumber = "";
+appformnumber  = docBIF.Element("form")?.Attribute("FormType")?.Value;
+var docSRF = new XDocument();
+docSRF.Add(new XElement("Application"
+, new XElement("forms", 
+new XElement("action")
+)
+));
+docSRF.XPathSelectElement("/Application/forms/action")?.SetAttributeValue("type","add");
+docSRF.XPathSelectElement("/Application/forms/action")?.Add(new XElement("form"));
+docSRF.XPathSelectElement("/Application/forms/action/form")?.SetAttributeValue("appformnumber", appformnumber);
+
+Console.WriteLine(docSRF.ToString());
